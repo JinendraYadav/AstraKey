98b6814db534a652f5e0d2883fc637b36bc69936
Jinendra Yadav <yadavjinendra123@gmail.com>
2025-07-04 23:07:13 +0530

    image fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
       <ul className="flex justify-between items-center px-6 py-3 max-w-screen-xl mx-auto">
         {/* Logo */}
         <li className="flex items-center font-extrabold text-2xl md:text-3xl tracking-wide">
-          <img src="src\assets\Logo.svg" alt="" className="w-12 h-12" />
+          <img src="public\Logo.svg" alt="" className="w-12 h-12" />
           <div className="flex text-center">
             <span className="text-[var(--color-secondary)]">Astra </span>
             <span className="text-[var(--color-primary)]"> Key</span>
@@ -20,7 +20,7 @@ const Navbar = () => {
             target="_blank"
             className="flex items-center gap-2 bg-[var(--color-secondary)] text-white ring-2 ring-[var(--color-primary)] text-sm md:text-base font-medium px-4 py-2 rounded-full hover:bg-[#112D4E] transition"
           >
-            <img src="src/assets/github.svg" alt="GitHub" className="w-5 h-5" />
+            <img src="public\github.svg" alt="GitHub" className="w-5 h-5" />
             GitHub
           </a>
         </li>
